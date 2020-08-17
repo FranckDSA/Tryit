317bc3d78babd23e4cf58ed4485e45b131af72db
Penelope des Dorides <penelopedelaville@gmail.com>
2020-08-17 17:47:47 +0200

    Car controller done

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,25 +7,34 @@ class CarsController < ApplicationController
     @car = Car.find(params[:id])
   end
 
-  # def new
-  #   @car = Car.new
-  # end
+  def new
+    @car = Car.new
+    @user = User.find(params[:user_id])
+  end
 
-  # def create
-  #   @car = Car.new(params_car)
-  #   @care.save
-  #   redirect_to car_path(@car)
-  # end
+  def create
+    @car = Car.new(params_car)
+    @car.user = User.find(params[:user_id])
+    @car.save
+    redirect_to car_path(@car)
+  end
 
-  # def edit
-  #   @car = Car.find(params[:id])
-  # end
+  def edit
+    @car = Car.find(params[:id])
+  end
 
-  # def update
-  #   @car = Car.find(params[:id])
-  #   @car.update(params_car)
-  #   redirect_to car_path(@car)
-  # end
+  def update
+    @car = Car.find(params[:id])
+    @car.update(params_car)
+    redirect_to car_path(@car)
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to cars_path
+    # redirect_to dashboard
+  end
 
   private
 
