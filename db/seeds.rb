06b7798441d6a61de3fe6f305d4cda1308c28e3a
Franck De Sousa <franck.dsa@gmail.com>
2020-08-19 12:28:54 +0200

    design flatpickr

--- db/seeds.rb
+++ db/seeds.rb
@@ -73,3 +73,60 @@ CATEGORIES = ["Family", "Sports", "Urban", "Vintage", "Exotic"]
   car.save!
 
 end
+
+12.times do
+
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  user_name = "#{first_name.downcase}_#{last_name.downcase}"
+
+  user = User.new(
+    first_name: first_name,
+    last_name: last_name,
+    username: user_name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    email: "#{first_name.downcase}.#{last_name.downcase}@gmail.com",
+    address: Faker::Address.full_address,
+    password: '123456'
+  )
+
+  p Faker::Avatar.image
+  file = URI.open(Faker::Avatar.image)
+
+  user.photo.attach(io: file, filename: 'avatar.jpg', content_type: 'image/jpg')
+  user.save!
+
+  brand = Faker::Vehicle.make
+
+  car = Car.new(
+      category: CATEGORIES.sample,
+      brand: brand,
+      model: Faker::Vehicle.model(make_of_model: brand),
+      motorizing: Faker::Vehicle.engine,
+      description:Faker::Vehicle.standard_specs.join(" | "),
+      start_date: Date.new,
+      end_date: Faker::Date.forward(days: 23),
+      user_id: user.id
+  )
+
+  file_car1 = URI.open('https://source.unsplash.com/800x450/?car')
+  car.photos.attach(io: file_car1, filename: 'car.jpg', content_type: 'image/jpg')
+  car.save!
+
+  file_car2 = URI.open('https://source.unsplash.com/800x450/?auto')
+  car.photos.attach(io: file_car2, filename: 'car.jpg', content_type: 'image/jpg')
+  car.save!
+
+  file_car3 = URI.open('https://source.unsplash.com/800x450/?voiture')
+  car.photos.attach(io: file_car3, filename: 'car.jpg', content_type: 'image/jpg')
+  car.save!
+
+  file_car4 = URI.open('https://source.unsplash.com/800x450/?road')
+  car.photos.attach(io: file_car4, filename: 'car.jpg', content_type: 'image/jpg')
+  car.save!
+
+  file_car5 = URI.open('https://source.unsplash.com/800x450/?garage')
+  car.photos.attach(io: file_car5, filename: 'car.jpg', content_type: 'image/jpg')
+  car.save!
+
+end
