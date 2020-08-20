36a6d5551563851a95a411cafabca462dec4d2f9
Franck De Sousa <franck.dsa@gmail.com>
2020-08-20 11:50:45 +0200

    ajout liens dashboard dans la navbar et d'un dashboard primitive

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create] do
-    resources :cars, only: [:new, :create]
+  resources :users, only: [:new, :create, :show] do
+    resources :cars, only: [:new, :create, :edit]
   end
 
   resources :cars, only: [:index, :show, :edit, :update, :destroy] do
