d02ad10b21cd2f92f2306ffe18b3e3ffb6576bd7
Alexandre Macquin <alexandre.macquin@gmail.com>
2020-08-20 11:34:49 +0200

    perfomed

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create] do
-    resources :cars, only: [:new, :create]
+    resources :cars, only: [:new, :create, :edit, :update, :destroy]
   end
 
-  resources :cars, only: [:index, :show, :edit, :update, :destroy] do
+  resources :cars, only: [:index, :show] do
     resources :bookings, only: [:new, :create]
   end
 
